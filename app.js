4562d6824fd1cb453562e378e5d39c3d14ea4abc
jonathan-crepeau <jon.crepeau@gmail.com>
2022-02-03 21:37:33 -0800

    Flexbox - parent through flex-flow completed

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+// NOTE - SOURCE: https://docs.google.com/document/d/1FRe9x3_sI10cyWfBrOON4-vu_M5SCHTD2mrml5Z9-QA/edit
+
